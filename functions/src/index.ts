f86da56c0e6bcab7ff96a0e95d873901b9f05c5d
Murat Demir <hisupermurat@gmail.com>
2018-09-11 19:38:46 +0300

    Thanks to @aaronte. Based on https://github.com/aaronte/angular-universal-firebase Sample Doc : https://hackernoon.com/deploying-angular-universal-v6-with-firebase-c86381ddd445 I didn't fork because this repository won't be just a sample and changed only a few things.

---
+++ functions/src/index.ts
@@ -0,0 +1,47 @@
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+
+require('zone.js/dist/zone-node');
+
+const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path');
+const { enableProdMode } = require('@angular/core');
+const { renderModuleFactory } = require('@angular/platform-server');
+
+const { AppServerModuleNgFactory } = require('../dist/server/main');
+
+enableProdMode();
+
+const index = require('fs')
+    .readFileSync(path.resolve(__dirname, '../dist/browser/index.html'), 'utf8')
+    .toString();
+
+const app = express();
+
+app.get('**', function(req, res) {
+    renderModuleFactory(AppServerModuleNgFactory, {
+        url: req.path,
+        document: index
+    }).then(html => res.status(200).send(html));
+});
+
+exports.ssr = functions.https.onRequest(app);
+
+export const getTimeByURL = functions.https.onRequest((request, response) => {
+    //response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    //response.set('Vary', 'Accept-Encoding, X-My-Custom-Header');
+    const currentTime = new Date();
+    response.status(200).send(`
+    <!doctype html>
+        <head>
+          <title>Time</title>
+        </head>
+        <body>
+          Current Time At Server : ${currentTime}
+        </body>
+    </html>`);
+
+    //response.send("Hello from Firebase!");
+});
