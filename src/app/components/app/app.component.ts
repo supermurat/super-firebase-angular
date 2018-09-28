990fcb4eea78c98f3d07f4bec311e1cb161c833e
supermurat <hisupermurat@gmail.com>
2018-09-28 14:46:06 +0300

    css files renamed as scss.

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -6,7 +6,7 @@ import { isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   renderer = '';
