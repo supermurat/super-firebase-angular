3b06fd2a9c65f9a1f969093acc63a2d7e84dc1fe
supermurat <hisupermurat@gmail.com>
2018-10-01 11:04:02 +0300

    Removed create new blog feature.

--- src/app/components/blog-list/blog-list.component.ts
+++ src/app/components/blog-list/blog-list.component.ts
@@ -16,8 +16,6 @@ export class BlogListComponent implements OnInit {
     title = 'Blog App';
     description = 'This App is in development!';
 
-    newBlog = new Blog();
-
     constructor(private afs: AngularFirestore,
                 private seo: SeoService,
                 private router: Router) {
@@ -30,12 +28,6 @@ export class BlogListComponent implements OnInit {
             title: this.title,
             description: this.description
         });
-    }
-
-    async create() {
-        const path = `blogs/${this.newBlog.name.toLowerCase()}`;
-        await this.afs.doc(path).set(this.newBlog);
-        this.router.navigate([path]);
     }
 
 }
