0b575700fc1fd660fbf0ac53f12e18c1c8fb810c
Murat Demir <hisupermurat@gmail.com>
2018-10-08 21:28:36 +0300

    fix: unit test issue

--- src/app/components/blog-list/blog-list.component.spec.ts
+++ src/app/components/blog-list/blog-list.component.spec.ts
@@ -2,13 +2,24 @@ import { async, TestBed } from '@angular/core/testing';
 
 import { RouterTestingModule } from '@angular/router/testing';
 import { BlogListComponent } from './blog-list.component';
-import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { AlertService, SeoService } from '../../services';
-import { AngularFireModule } from '@angular/fire';
-import { firebaseConfig } from '../../../environments/firebase.config';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireStorageModule } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
+import { Blog } from '../../models/blog';
+import 'rxjs-compat/add/observable/from';
+
+const testData: Array<Array<Blog>> = [[
+    { name: 'first-block', bio: 'this is good sample', imgName: 'bad, very bad angel.gif', imgURL: undefined},
+    { name: 'second-block', bio: 'this is better sample', imgName: 'bad, very bad angel.gif', imgURL: undefined}
+]];
+
+const angularFirestoreStub = {
+    collection: jasmine.createSpy('collection').and.returnValue(
+    {
+        valueChanges: jasmine.createSpy('valueChanges').and.returnValue(Observable.from(testData))
+    })
+};
 
 describe('BlogListComponent', () => {
     beforeEach(async(() => {
@@ -17,14 +28,11 @@ describe('BlogListComponent', () => {
                 BlogListComponent
             ],
             providers: [
-                AlertService, SeoService, AngularFirestore
+                AlertService, SeoService,
+                { provide: AngularFirestore, useValue: angularFirestoreStub }
             ],
             imports: [
-                RouterTestingModule.withRoutes([{path: 'blog', component: BlogListComponent}]),
-                AngularFireModule.initializeApp(firebaseConfig),
-                AngularFirestoreModule, // imports firebase/firestore, only needed for database features
-                AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-                AngularFireStorageModule // imports firebase/storage only needed for storage features
+                RouterTestingModule.withRoutes([{path: 'blog', component: BlogListComponent}])
             ]
         })
             .compileComponents();
@@ -50,6 +58,17 @@ describe('BlogListComponent', () => {
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('h2').textContent)
             .toContain('Blog Entries');
+    }));
+
+    it('count of blog should be 2', async(() => {
+        const fixture = TestBed.createComponent(BlogListComponent);
+        const app = fixture.debugElement.componentInstance;
+        fixture.detectChanges();
+        app.blogs$.subscribe(result => {
+            expect(result.length)
+                .toEqual(2);
+        });
+        fixture.detectChanges();
     }));
 
 });
