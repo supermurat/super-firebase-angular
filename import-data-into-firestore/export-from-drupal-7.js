1914baaef163ea95a846673521b9f1c1c664ed23
supermurat <hisupermurat@gmail.com>
2018-11-12 18:22:36 +0300

    fix: an issue about exporting blogs from drupal

--- import-data-into-firestore/export-from-drupal-7.js
+++ import-data-into-firestore/export-from-drupal-7.js
@@ -13,11 +13,11 @@ let totalOfItems = 1;
 let countOfCompletedItems = 0;
 
 function fixDocID(docID) {
-    return docID.replace(/gunluk\//gi, '').replace(/\//gi, '-').replace(/\\/gi, '-');
+    return docID.replace(/blog\//gi, '').replace(/gunluk\//gi, '').replace(/\//gi, '-').replace(/\\/gi, '-');
 }
 
 connection.query(
-    "SELECT u.language, u.alias, n.nid, n.type, n.status, FROM_UNIXTIME(n.changed) AS 'changed', FROM_UNIXTIME(n.created) AS 'created', n.title FROM node n LEFT JOIN url_alias u ON u.source = CONCAT('node/', n.nid) WHERE n.type='blog' AND n.status='1'",
+    "SELECT u.language, u.alias, n.nid, n.type, n.status, FROM_UNIXTIME(n.changed) AS 'changed', FROM_UNIXTIME(n.created) AS 'created', n.title, fdb.body_value AS 'content' FROM node n LEFT JOIN url_alias u ON u.source = CONCAT('node/', n.nid) AND u.language=n.language AND u.pid IN (SELECT MAX(stu.pid) FROM url_alias stu GROUP BY stu.source) LEFT JOIN field_data_body fdb ON fdb.entity_type = 'node' AND fdb.entity_id = n.nid AND fdb.revision_id = n.vid WHERE n.type='blog' AND n.status='1'",
     function (error, results, fields) {
         if (error) throw error;
 
