8cc2dab175d1751f9ee4f9cfc9a5aa0b81eb0266
supermurat <hisupermurat@gmail.com>
2018-11-15 14:41:20 +0300

    feat: pages and stories exported from drupal 7

--- migrate-to-firestore/export-from-drupal-7.js
+++ migrate-to-firestore/export-from-drupal-7.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql');
 const fs = require('fs');
+const latinize = require('latinize');
+
 const mysqlConfig = require("./mysql-config.json");
 
 const connection = mysql.createConnection(mysqlConfig);
@@ -7,13 +9,21 @@ const connection = mysql.createConnection(mysqlConfig);
 connection.connect();
 
 const resultsForFirestore = {};
+let totalOfCollections = 2;
+let countOfCompletedCollections = 0;
+
 const collectionNameOfBlogs = "blogs";
 resultsForFirestore[collectionNameOfBlogs] = {};
-let totalOfItems = 1;
-let countOfCompletedItems = 0;
+const collectionNameOfPages = "pages";
+resultsForFirestore[collectionNameOfPages] = {};
 
 function fixDocID(docID) {
-    return docID.replace(/blog\//gi, '').replace(/gunluk\//gi, '').replace(/\//gi, '-').replace(/\\/gi, '-');
+    docID = latinize(docID);
+    return docID.toLocaleLowerCase()
+        .replace(/ı\//gi, 'i')
+        .replace(/blog\//gi, '').replace(/gunluk\//gi, '')
+        .replace(/story\//gi, '').replace(/makale\//gi, '')
+        .replace(/\//gi, '-').replace(/\\/gi, '-');
 }
 
 connection.query(
@@ -22,20 +32,33 @@ connection.query(
         if (error) throw error;
 
         results.forEach(function(element) {
-            const docID = fixDocID(element.alias); // TODO: set document ID for firestore
+            const docID = fixDocID(element.alias);
             resultsForFirestore[collectionNameOfBlogs][docID] = element;
         });
-        countOfCompletedItems++;
+        countOfCompletedCollections++;
         writeResultToFile();
 });
+
+connection.query(
+    "SELECT u.language, u.alias, n.nid, n.type, n.status, FROM_UNIXTIME(n.changed) AS 'changed', FROM_UNIXTIME(n.created) AS 'created', n.title, fdb.body_value AS 'content' FROM node n LEFT JOIN url_alias u ON u.source = CONCAT('node/', n.nid) AND u.language=n.language AND u.pid IN (SELECT MAX(stu.pid) FROM url_alias stu GROUP BY stu.source) LEFT JOIN field_data_body fdb ON fdb.entity_type = 'node' AND fdb.entity_id = n.nid AND fdb.revision_id = n.vid WHERE n.type IN ('page', 'story') AND n.status='1'",
+    function (error, results, fields) {
+        if (error) throw error;
+
+        results.forEach(function(element) {
+            const docID = fixDocID(element.alias);
+            resultsForFirestore[collectionNameOfPages][docID] = element;
+        });
+        countOfCompletedCollections++;
+        writeResultToFile();
+    });
 
 connection.end();
 
 function writeResultToFile() {
-    if (countOfCompletedItems === totalOfItems){
+    if (countOfCompletedCollections === totalOfCollections){
         fs.writeFile('data.json', JSON.stringify(resultsForFirestore, undefined, 2), {encoding: 'utf8', flag: 'w'}, function (err) {
             if (err) throw err;
-            console.log('Exported collection count:' + countOfCompletedItems);
+            console.log('Exported collection count:' + countOfCompletedCollections);
         });
     }
 }
