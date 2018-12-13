8daa442b0852de757669f08dd67c9ffa0eaa537f
Murat Demir <murat@kozayazilim.com>
2018-12-13 09:26:41 +0300

    fix: orderNo issue of drupal exporter fix: timestamp issue of migrate-to-firebase

--- migrate-to-firebase/export-from-drupal-7.js
+++ migrate-to-firebase/export-from-drupal-7.js
@@ -40,10 +40,19 @@ function checkDirectory(directoryPath) {
 }
 
 function fixDocID(docID) {
-    docID = latinize(docID);
-    return docID//.toLocaleLowerCase()
-        //.replace(/ı\//gi, 'i')
-        .replace(/\//gi, '-').replace(/\\/gi, '-');
+    return latinize(docID).replace(/\//gi, '-').replace(/\\/gi, '-');
+}
+
+function addToBlogs(lang, docID, element) {
+    resultsForFirestore[collectionNameOfBlogs + lang][docID] = { ...element };
+    resultsForFirestore[collectionNameOfBlogs + lang][docID].orderNo
+        = (Object.keys(resultsForFirestore[collectionNameOfBlogs + lang]).length) * -1;
+}
+
+function addToPages(lang, docID, element) {
+    resultsForFirestore[collectionNameOfPages + lang][docID] = { ...element };
+    resultsForFirestore[collectionNameOfPages + lang][docID].orderNo
+        = (Object.keys(resultsForFirestore[collectionNameOfPages + lang]).length) * -1;
 }
 
 function addToRedirectionRecords(lang, alias, path, docID) {
@@ -81,30 +90,27 @@ function downloadFiles(htmlContent) {
 
 function getBlogs() {
     connection.query(
-        "SELECT u.language, u.alias, n.nid, n.type, n.status, FROM_UNIXTIME(n.changed) AS 'changed', FROM_UNIXTIME(n.created) AS 'created', n.title, fdb.body_value AS 'content' FROM node n LEFT JOIN url_alias u ON u.source = CONCAT('node/', n.nid) AND u.language=n.language AND u.pid IN (SELECT MAX(stu.pid) FROM url_alias stu GROUP BY stu.source) LEFT JOIN field_data_body fdb ON fdb.entity_type = 'node' AND fdb.entity_id = n.nid AND fdb.revision_id = n.vid WHERE n.type='blog' AND n.status='1' ORDER BY n.changed ASC",
+        "SELECT u.language, u.alias, n.nid, n.type, n.status, FROM_UNIXTIME(n.changed) AS 'changed', FROM_UNIXTIME(n.created) AS 'created', n.title, fdb.body_value AS 'content' FROM node n LEFT JOIN url_alias u ON u.source = CONCAT('node/', n.nid) AND u.language=n.language AND u.pid IN (SELECT MAX(stu.pid) FROM url_alias stu GROUP BY stu.source) LEFT JOIN field_data_body fdb ON fdb.entity_type = 'node' AND fdb.entity_id = n.nid AND fdb.revision_id = n.vid WHERE n.type='blog' AND n.status='1' ORDER BY n.created ASC",
         function (error, results, fields) {
             if (error) throw error;
 
-            let i = 1;
             results.forEach(function(element) {
                 const docID = fixDocID(element.alias.replace(/blog\//gi, '').replace(/gunluk\//gi, ''));
-                element.orderNo = i * -1;
                 element.createdBy = "Murat Demir";
                 element.i18nKey = docID; // i18nKey should be matched with translations
                 downloadFiles(element.content);
                 if (element.language === "und") {
-                    resultsForFirestore[collectionNameOfBlogs + "_tr-TR"][docID] = element;
-                    resultsForFirestore[collectionNameOfBlogs + "_en-US"][docID] = element;
+                    addToBlogs("_tr-TR", docID, element);
+                    addToBlogs("_en-US", docID, element);
                     addToRedirectionRecords("tr/", element.alias, "gunluk/", docID);
                     addToRedirectionRecords("en/", element.alias, "blog/", docID);
                 } else if (element.language === "tr") {
-                    resultsForFirestore[collectionNameOfBlogs + "_tr-TR"][docID] = element;
+                    addToBlogs("_tr-TR", docID, element);
                     addToRedirectionRecords("tr/", element.alias, "gunluk/", docID);
                 } else if (element.language === "en") {
-                    resultsForFirestore[collectionNameOfBlogs + "_en-US"][docID] = element;
+                    addToBlogs("_en-US", docID, element);
                     addToRedirectionRecords("en/", element.alias, "blog/", docID);
                 }
-                i++;
             });
             getPages();
         });
@@ -112,30 +118,27 @@ function getBlogs() {
 
 function getPages() {
     connection.query(
-        "SELECT u.language, u.alias, n.nid, n.type, n.status, FROM_UNIXTIME(n.changed) AS 'changed', FROM_UNIXTIME(n.created) AS 'created', n.title, fdb.body_value AS 'content' FROM node n LEFT JOIN url_alias u ON u.source = CONCAT('node/', n.nid) AND u.language=n.language AND u.pid IN (SELECT MAX(stu.pid) FROM url_alias stu GROUP BY stu.source) LEFT JOIN field_data_body fdb ON fdb.entity_type = 'node' AND fdb.entity_id = n.nid AND fdb.revision_id = n.vid WHERE n.type IN ('page', 'story') AND n.status='1' ORDER BY n.changed ASC",
+        "SELECT u.language, u.alias, n.nid, n.type, n.status, FROM_UNIXTIME(n.changed) AS 'changed', FROM_UNIXTIME(n.created) AS 'created', n.title, fdb.body_value AS 'content' FROM node n LEFT JOIN url_alias u ON u.source = CONCAT('node/', n.nid) AND u.language=n.language AND u.pid IN (SELECT MAX(stu.pid) FROM url_alias stu GROUP BY stu.source) LEFT JOIN field_data_body fdb ON fdb.entity_type = 'node' AND fdb.entity_id = n.nid AND fdb.revision_id = n.vid WHERE n.type IN ('page', 'story') AND n.status='1' ORDER BY n.created ASC",
         function (error, results, fields) {
             if (error) throw error;
 
-            let i = 1;
             results.forEach(function(element) {
                 const docID = fixDocID(element.alias.replace(/story\//gi, '').replace(/makale\//gi, ''));
-                element.orderNo = i * -1;
                 element.createdBy = "Murat Demir";
                 element.i18nKey = docID; // i18nKey should be matched with translations
                 downloadFiles(element.content);
                 if (element.language === "und") {
-                    resultsForFirestore[collectionNameOfPages + "_tr-TR"][docID] = element;
-                    resultsForFirestore[collectionNameOfPages + "_en-US"][docID] = element;
+                    addToPages("_tr-TR", docID, element);
+                    addToPages("_en-US", docID, element);
                     addToRedirectionRecords("tr/", element.alias, "sayfa/", docID);
                     addToRedirectionRecords("en/", element.alias, "page/", docID);
                 } else if (element.language === "tr") {
-                    resultsForFirestore[collectionNameOfPages + "_tr-TR"][docID] = element;
+                    addToPages("_tr-TR", docID, element);
                     addToRedirectionRecords("tr/", element.alias, "sayfa/", docID);
                 } else if (element.language === "en") {
-                    resultsForFirestore[collectionNameOfPages + "_en-US"][docID] = element;
+                    addToPages("_en-US", docID, element);
                     addToRedirectionRecords("en/", element.alias, "page/", docID);
                 }
-                i++;
             });
             writeResultToFile();
         });
