fe0a0857c3dff2015c126c8fbdc1eca351a5e706
Murat Demir <hisupermurat@gmail.com>
2018-12-29 21:39:30 +0300

    fix: changed tslint rules of functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,38 +1,46 @@
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-//
+/**
+ * Start writing Firebase Functions
+ * https://firebase.google.com/docs/functions/typescript
+ */
 
-require('zone.js/dist/zone-node');
+// tslint:disable-next-line:no-import-side-effect
+import 'zone.js/dist/zone-node';
 
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
-const express = require('express');
-const path = require('path');
-const { enableProdMode } = require('@angular/core');
-const { renderModuleFactory } = require('@angular/platform-server');
-const { readFileSync, existsSync } = require('fs');
+import { enableProdMode } from '@angular/core';
+import { renderModuleFactory } from '@angular/platform-server';
+import * as express from 'express';
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+import { existsSync, readFileSync } from 'fs';
+import * as path from 'path';
 
 admin.initializeApp(functions.config().firebase);
-admin.app().firestore().settings({timestampsInSnapshots: true});
+admin.app()
+    .firestore()
+    .settings({timestampsInSnapshots: true});
 const db = admin.firestore();
-let serverMain;
-let serverEN;
-let serverTR;
-let indexMain;
-let indexEN;
-let indexTR;
+
+let serverMain: any;
+let serverEN: any;
+let serverTR: any;
+let indexMain: any;
+let indexEN: any;
+let indexTR: any;
 
 const uniqueKeyFor404 = '"doNotRemoveMe-ThisIsForOnlyGet404ErrorOnSSRWithUniqueAndHiddenKey"';
 const isI18N = !existsSync(path.resolve(__dirname, '../dist/browser/index.html'));
 
 if (isI18N) {
+    // tslint:disable-next-line:no-var-requires no-require-imports
     serverEN = require('../dist/server/en/main');
+    // tslint:disable-next-line:no-var-requires no-require-imports
     serverTR = require('../dist/server/tr/main');
     indexEN = readFileSync(path.resolve(__dirname, '../dist/browser/en/index.html'), 'utf8')
         .toString();
     indexTR = readFileSync(path.resolve(__dirname, '../dist/browser/tr/index.html'), 'utf8')
         .toString();
 } else {
+    // tslint:disable-next-line:no-var-requires no-require-imports
     serverMain = require('../dist/server/main');
     indexMain = readFileSync(path.resolve(__dirname, '../dist/browser/index.html'), 'utf8')
         .toString();
@@ -42,57 +50,75 @@ enableProdMode();
 
 const app = express();
 
-function getDynamicFile(req, res){
-    const fileName = req.url.replace(/\//gi, "");
-    const fileExt = '.' + fileName.split(".")[1];
+/** Redirection Record Model */
+class RedirectionRecord {
+    /** http code */
+    code?: number;
+    /** url to redirect */
+    url?: string;
+}
+
+const getDynamicFile = (req: express.Request, res: express.Response) => {
+    const fileName = req.url.replace(/\//gi, '');
+    const fileExt = `.${fileName.split('.')[1]}`;
     db.collection('dynamicFiles').doc(fileName).get()
         .then((snapshot) => {
             if (!snapshot.exists) {
                 console.log('No such file:', fileName);
-                res.status(404).send('404 : No such file:' + fileName);
+                res.status(404)
+                    .send(`404 : No such file:${fileName}`);
             } else {
-                res.status(200).type(fileExt).send(snapshot.data().content.replace(/\\r\\n/g, "\r\n"));
+                res.status(200)
+                    .type(fileExt)
+                    .send(snapshot.data().content.replace(/\\r\\n/g, '\r\n'));
             }
         })
         .catch((err) => {
             console.log('Error in getDynamicFile:', err);
-            res.status(500).send(err);
+            res.status(500)
+                .send(err);
         });
-}
+};
 
-function getSSR(req, res){
+const getSSR = (req: express.Request, res: express.Response) => {
     const supportedLocales = ['en', 'tr'];
     const defaultLocale = 'en';
     const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
-    //check if the requested url has a correct format '/locale' and matches any of the supportedLocales
+    // check if the requested url has a correct format '/locale' and matches any of the supportedLocales
     const locale = (matches && supportedLocales.indexOf(matches[1]) !== -1) ? matches[1] : defaultLocale;
 
     const index = !isI18N ? indexMain : locale ===  'tr' ? indexTR : indexEN;
     const server = !isI18N ? serverMain : locale ===  'tr' ? serverTR : serverEN;
     renderModuleFactory(server.AppServerModuleNgFactory, {
         url: req.path,
-        document: index
-    }).then(html =>
-        {
+        document: index,
+    }).then((html) => {
             const newUrlInfo = html.match(/--http-redirect-301--[\w\W]*--end-of-http-redirect-301--/gi);
-            if (newUrlInfo){
+            if (newUrlInfo) {
                 const newUrl = newUrlInfo[0]
-                    .replace(/--end-of-http-redirect-301--/gi, "")
-                    .replace(/--http-redirect-301--/gi, "");
+                    .replace(/--end-of-http-redirect-301--/gi, '')
+                    .replace(/--http-redirect-301--/gi, '');
                 res.redirect(301, newUrl);
-            } else
+            } else {
             // thing about redirect to 404 but maybe keeping current url is more cool
-                res.status(html.indexOf(uniqueKeyFor404) > -1 ? 404 : 200).send(html);
-        }
-    );
-}
+                res.status(html.indexOf(uniqueKeyFor404) > -1 ? 404 : 200)
+                    .send(html);
+            }
+        },
+    ).catch((err) => {
+        console.log('Error in getSSR:', err);
+    });
+};
 
-function checkRedirection(req, res){
-    return new Promise((resolve, reject) => {
-        const url = req.url.substring(1).replace(/\//gi, "\\"); // firestore doesn't allow "/" to be in document ID
+const checkRedirection = async (req: express.Request, res: express.Response) =>
+    new Promise<RedirectionRecord>((resolve, reject) => {
+        // firestore doesn't allow "/" to be in document ID
+        const url = req.url.substring(1).replace(/\//gi, '\\');
         const documentID = decodeURIComponent(url);
-        if (documentID)
-            db.collection('redirectionRecords').doc(documentID).get()
+        if (documentID) {
+            db.collection('redirectionRecords')
+                .doc(documentID)
+                .get()
                 .then((snapshot) => {
                     if (!snapshot.exists) {
                         console.log('!snapshot.exists:', documentID);
@@ -105,18 +131,18 @@ function checkRedirection(req, res){
                     console.log('Error in checkRedirection:', err);
                     resolve(); // let's handle this error with SSR
                 });
-        else
+        } else {
             resolve();
+        }
     });
-}
 
-app.get('**', function(req, res) {
+app.get('**', (req: express.Request, res: express.Response) => {
     const matchesDynamicFiles = req.url.match(/^\/([a-zA-Z0-9]*(\.)[a-zA-Z0-9]*)$/);
     if (matchesDynamicFiles && req.url.length > 3) {
         getDynamicFile(req, res);
     } else {
         checkRedirection(req, res)
-            .then((redirectionRecord : { code: string, url: string }) => {
+            .then((redirectionRecord: RedirectionRecord) => {
                 if (redirectionRecord) {
                     console.log('redirectionRecord:', redirectionRecord);
                     res.redirect(redirectionRecord.code, redirectionRecord.url);
