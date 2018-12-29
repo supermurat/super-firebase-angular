b92c90739ff385d65df998d793c9de210797d0dd
Murat Demir <hisupermurat@gmail.com>
2018-12-29 22:22:57 +0300

    fix: changed tslint rules of migrate-to-firebase

--- migrate-to-firebase/src/export-from-mysql.ts
+++ migrate-to-firebase/src/export-from-mysql.ts
@@ -1,34 +1,39 @@
-import * as mysql from 'mysql';
 import * as fs from 'fs';
+import * as mysql from 'mysql';
 import * as path from 'path';
 
-const mysqlConfig = require("../mysql-config.json");
+// tslint:disable-next-line:no-var-requires no-require-imports
+const mysqlConfig = require('../mysql-config.json');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-const pathOfData = path.dirname(__dirname) + path.sep + "data";
-const pathOfDataJson = pathOfData + path.sep + "data.json";
-if (!fs.existsSync(pathOfData))
+const pathOfData = `${path.dirname(__dirname) + path.sep}data`;
+const pathOfDataJson = `${pathOfData + path.sep}data.json`;
+if (!fs.existsSync(pathOfData)) {
     fs.mkdirSync(pathOfData);
+}
 
 connection.connect();
 
 connection.query(
     "SELECT 'random1' AS 'id', CURRENT_TIMESTAMP() AS 'testDate'", // TODO: set your own mysql query
-    function (error, results, fields) {
-        if (error) throw error;
+    (error, results, fields) => {
+        if (error) { throw error; }
 
-        const collectionName = "myCollection"; // TODO: set collection name for firestore
+        const collectionName = 'myCollection'; // TODO: set collection name for firestore
         const resultsForFirestore = {};
         resultsForFirestore[collectionName] = {};
-        results.forEach(function(element) {
+        results.forEach((element) => {
             const docID = element.id; // TODO: set document ID for firestore
             resultsForFirestore[collectionName][docID] = element;
         });
 
-        fs.writeFile(pathOfDataJson, JSON.stringify(resultsForFirestore, undefined, 2), {encoding: 'utf8', flag: 'w'}, function (err) {
-            if (err) throw err;
-            console.log('Exported row count:' + results.length);
+        fs.writeFile(
+            pathOfDataJson,
+            JSON.stringify(resultsForFirestore, undefined, 2),
+            {encoding: 'utf8', flag: 'w'}, (err) => {
+            if (err) { throw err; }
+            console.log(`Exported row count:${results.length}`);
         });
 });
 
--- migrate-to-firebase/src/migrate-to-firebase.ts
+++ migrate-to-firebase/src/migrate-to-firebase.ts
@@ -1,53 +1,56 @@
+import { Storage } from '@google-cloud/storage';
 import * as admin from 'firebase-admin';
 import * as fs from 'fs';
 import * as mime from 'mime-types';
 import * as path from 'path';
-import { Storage } from '@google-cloud/storage';
 
-const serviceAccount = require("../supermurat-com-service-key.json");
+// tslint:disable-next-line:no-var-requires no-require-imports
+const serviceAccount = require('../supermurat-com-service-key.json');
 
-let data = require("../data/data.json");
+// tslint:disable-next-line:no-var-requires no-require-imports
+let data = require('../data/data.json');
 
 const storage = new Storage();
 
 // CONFIG
 // keep "remoteFilePath" undefined in order to use relative path
 // think about to give read permission for your users or public : "../firebase/storage.rules"
-const remoteFilePath = "/publicFiles/";
+const remoteFilePath = '/publicFiles/';
 // END OF CONFIG
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://" + serviceAccount.project_id + ".firebaseio.com"
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`,
 });
 admin.app().firestore().settings({timestampsInSnapshots: true});
-const bucketName = serviceAccount.project_id + ".appspot.com";
+const bucketName = `${serviceAccount.project_id}.appspot.com`;
 const bucket = admin.storage().bucket(bucketName);
-const pathOfData = path.dirname(__dirname) + path.sep + "data";
-const pathOfFiles = pathOfData + path.sep + "files";
+const pathOfData = `${path.dirname(__dirname) + path.sep}data`;
+const pathOfFiles = `${pathOfData + path.sep}files`;
 
 // FixFilePaths
-function getFiles(dir, _files){
-    const files_ = _files || [];
-    const files = fs.readdirSync(dir);
-    for (const i in files){
-        const name = dir + path.sep + files[i];
-        if (fs.statSync(name).isDirectory()){
-            getFiles(name, files_);
+const getFiles = (dir: string, files: string[]) => {
+    const filesWithBefore = files || [];
+    const currentFiles = fs.readdirSync(dir);
+    for (const currentFile of currentFiles) {
+        const name = String(dir + path.sep + currentFile);
+        if (fs.statSync(name).isDirectory()) {
+            getFiles(name, filesWithBefore);
         } else {
-            files_.push(name);
+            filesWithBefore.push(name);
         }
     }
-    return files_;
-}
 
-function uploadImageToStorage(fileContent, fileName) {
-    return new Promise((resolve, reject) => {
+    return filesWithBefore;
+};
+
+const uploadImageToStorage = async (fileContent: any, fileName: string) =>
+    new Promise<any>((resolve, reject) => {
         const fileUpload = bucket.file(fileName);
         const blobStream = fileUpload.createWriteStream({
             metadata: {
-                contentType: mime.lookup(fileName)
-            }
+                contentType: mime.lookup(fileName),
+            },
         });
         blobStream.on('error', (error) => {
             reject(error);
@@ -55,63 +58,62 @@ function uploadImageToStorage(fileContent, fileName) {
         blobStream.on('finish', () => {
             fileUpload.acl.add({
                 entity: 'allUsers',
-                role: storage.acl.READER_ROLE
-            }).then(function(info) {
-                //console.log(info); // this "info" contains lots of cool info about file
-                resolve("https://storage.googleapis.com/" + bucket.name + fileName);
-            }).catch(err => {
+                role: storage.acl.READER_ROLE,
+            }).then((info) => {
+                // console.log(info); // this "info" contains lots of cool info about file
+                resolve(`https://storage.googleapis.com/${bucket.name}${fileName}`);
+            }).catch((err) => {
                 reject(err);
             });
         });
         blobStream.end(fileContent);
     });
-}
 
-function uploadFileAndGetURL(pathOfFile, filePath){
-    return new Promise((resolve, reject) => {
-        const relativePath = filePath.replace(pathOfFile, "").replace(/\\/g, "/");
+const uploadFileAndGetURL = async (pathOfFile: string, filePath: string) =>
+    new Promise<any>((resolve, reject) => {
+        const relativePath = filePath.replace(pathOfFile, '').replace(/\\/g, '/');
         const destinationPath = remoteFilePath ? remoteFilePath + path.basename(filePath) : relativePath;
 
         bucket.file(destinationPath).exists()
-            .then(function(info) {
+            .then((info) => {
                 if (info[0]) {
-                    const url = "https://storage.googleapis.com/" + bucket.name + destinationPath;
-                    console.log("Already Uploaded File : ", url);
-                    resolve({ relativePath: relativePath, url: url });
+                    const url = `https://storage.googleapis.com/${bucket.name}${destinationPath}`;
+                    console.log('Already Uploaded File : ', url);
+                    resolve({relativePath, url});
                 } else {
                     const fileContent = fs.readFileSync(filePath);
                     uploadImageToStorage(fileContent, destinationPath)
-                        .then(url => {
-                            console.log("Newly Uploaded File : ", url);
-                            resolve({ relativePath: relativePath, url: url });
+                        .then((url) => {
+                            console.log('Newly Uploaded File : ', url);
+                            resolve({relativePath, url});
                         })
-                        .catch(error => {
+                        .catch((error) => {
                             reject(error);
                         });
                 }
             })
-            .catch(error => {
+            .catch((error) => {
                 reject(error);
             });
     });
-}
 
-function uploadFilesAndFixFilePaths(){
-    return new Promise((resolve, reject) => {
+const uploadFilesAndFixFilePaths = async () =>
+    new Promise<any>((resolve, reject) => {
         let dataString = JSON.stringify(data);
-        if (!fs.existsSync(pathOfFiles))
-            resolve(); // There is no directory (pathOfFiles) to upload, so let's skip to import only data.json
+        if (!fs.existsSync(pathOfFiles)) {
+            resolve();
+        } // There is no directory (pathOfFiles) to upload, so let's skip to import only data.json
         const files = getFiles(pathOfFiles, undefined);
         if (files.length > 0) {
             const promises = [];
-            files.forEach(function (filePath) {
+            files.forEach((filePath) => {
                 promises.push(uploadFileAndGetURL(pathOfFiles, filePath));
             });
             Promise.all(promises)
                 .then((results) => {
-                    results.forEach(function (pru) {
+                    results.forEach((pru) => {
                         dataString = dataString.replace(new RegExp(pru.relativePath, 'gi'), pru.url);
-                        //console.log(pru.relativePath, ">>", pru.url);
+                        // console.log(pru.relativePath, ">>", pru.url);
                     });
                     data = JSON.parse(dataString);
                     resolve();
@@ -123,77 +125,82 @@ function uploadFilesAndFixFilePaths(){
             resolve();
         }
     });
-}
 // End OF FixFilePaths
 
-function fixTimestamps(nestedData){
-    if (typeof nestedData === "string") {
-        if (nestedData.match(/^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])[ T](00|[0-9]|0[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])\.\d\d\d[Z]?$/gi)) {
+const fixTimestamps = (nestedData: any) => {
+    if (typeof nestedData === 'string') {
+        // tslint:disable-next-line: max-line-length
+        if (nestedData.match(/^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])[ T](00|[0-9]|0[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])\.\d\d\d[Z]?$/gi)
+        ) {
             return new Date(nestedData);
         }
-    } else if (typeof nestedData === "object" && nestedData !== undefined && nestedData !== null) {
-        Object.keys(nestedData).forEach(key => {
+    } else if (typeof nestedData === 'object' && nestedData !== undefined && nestedData !== null) {
+        Object.keys(nestedData).forEach((key) => {
             nestedData[key] = fixTimestamps(nestedData[key]);
         });
     }
+
     return nestedData;
-}
+};
 
-function importIntoFirestore(){
-    data && Object.keys(data).forEach(key => {
-        const nestedContent = data[key];
+const importIntoFirestore = () => {
+    if (data) {
+        Object.keys(data).forEach((key) => {
+            const nestedContent = data[key];
 
-        if (typeof nestedContent === "object") {
-            Object.keys(nestedContent).forEach(docID => {
-                const nestedData = fixTimestamps(nestedContent[docID]);
-                const docData = {...nestedData};
-                Object.keys(nestedData).forEach(subKey => {
-                    if (subKey.startsWith("__collection__")) {
-                        delete docData[subKey];
-                    }
-                });
-
-                admin.firestore()
-                    .collection(key)
-                    .doc(docID)
-                    .set(docData)
-                    .then((res) => {
-                        Object.keys(nestedData).forEach(subKey => {
-                            if (subKey.startsWith("__collection__")) {
-                                console.log("Importing sub collections:", key, docID, subKey);
-                                const subNestedContent = nestedData[subKey];
-                                Object.keys(subNestedContent).forEach(subDocID => {
-                                    const subDocData = {...subNestedContent[subDocID]};
-                                    admin.firestore()
-                                        .collection(key)
-                                        .doc(docID)
-                                        .collection(subKey.replace("__collection__", ""))
-                                        .doc(subDocID)
-                                        .set(subDocData)
-                                        .then((subRes) => {
-                                            console.log("Imported:", key, docID, subKey, subDocID);
-                                        })
-                                        .catch((error) => {
-                                            console.error("Error:", key, docID, subKey, subDocID, error);
-                                        });
-                                });
-                            }
-                        });
-                        console.log("Imported:", key, docID);
-                    })
-                    .catch((error) => {
-                        console.error("Error:", key, docID, error);
+            if (typeof nestedContent === 'object') {
+                Object.keys(nestedContent).forEach((docID) => {
+                    const nestedData = fixTimestamps(nestedContent[docID]);
+                    const docData = {...nestedData};
+                    Object.keys(nestedData).forEach((subKey) => {
+                        if (subKey.startsWith('__collection__')) {
+                            // tslint:disable-next-line:no-dynamic-delete
+                            delete docData[subKey];
+                        }
                     });
 
-            });
-        }
-    });
-}
+                    admin.firestore()
+                        .collection(key)
+                        .doc(docID)
+                        .set(docData)
+                        .then((res) => {
+                            Object.keys(nestedData).forEach((subKey) => {
+                                if (subKey.startsWith('__collection__')) {
+                                    console.log('Importing sub collections:', key, docID, subKey);
+                                    const subNestedContent = nestedData[subKey];
+                                    Object.keys(subNestedContent).forEach((subDocID) => {
+                                        const subDocData = {...subNestedContent[subDocID]};
+                                        admin.firestore()
+                                            .collection(key)
+                                            .doc(docID)
+                                            .collection(subKey.replace('__collection__', ''))
+                                            .doc(subDocID)
+                                            .set(subDocData)
+                                            .then((subRes) => {
+                                                console.log('Imported:', key, docID, subKey, subDocID);
+                                            })
+                                            .catch((error) => {
+                                                console.error('Error:', key, docID, subKey, subDocID, error);
+                                            });
+                                    });
+                                }
+                            });
+                            console.log('Imported:', key, docID);
+                        })
+                        .catch((error) => {
+                            console.error('Error:', key, docID, error);
+                        });
+
+                });
+            }
+        });
+    }
+};
 
 uploadFilesAndFixFilePaths()
     .then(() => {
         importIntoFirestore();
     })
-    .catch(error => {
+    .catch((error) => {
         console.log(error);
     });
