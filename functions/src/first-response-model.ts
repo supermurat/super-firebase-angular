f79806184fffd6acbeb71773b475c1d93ebf34e6
supermurat <hisupermurat@gmail.com>
2019-01-14 14:41:18 +0300

    feat: ssr, redirection and dynamic file features have been improved

---
+++ functions/src/first-response-model.ts
@@ -0,0 +1,15 @@
+/** First Response Model */
+export class FirstResponseModel {
+    /** document id */
+    id?: string;
+    /** http code */
+    code?: number;
+    /** url to redirect */
+    url?: string;
+    /** type of content */
+    type?: string;
+    /** content of object */
+    content?: string;
+    /** expire date */
+    expireDate?: any;
+}
--- functions/src/redirection-record-model.ts
+++
@@ -1,7 +0,0 @@
-/** Redirection Record Model */
-export class RedirectionRecordModel {
-    /** http code */
-    code?: number;
-    /** url to redirect */
-    url?: string;
-}
