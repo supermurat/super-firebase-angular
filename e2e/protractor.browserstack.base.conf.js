969a8a7ad0463635bce307227e59853a73a15c42
Murat Demir <hisupermurat@gmail.com>
2019-04-17 21:09:53 +0300

    fix: this is a quick fix for e2e tests

--- e2e/protractor.browserstack.base.conf.js
+++ e2e/protractor.browserstack.base.conf.js
@@ -34,12 +34,12 @@ exports.config = {
         "browserName": "Chrome"
     }, {
         "browserName": "Firefox"
-    }, /*{
+    } /*,{
         "os" : "Windows",
         "os_version" : "7",
         "browserName": "IE",
         "browser_version" : "10.0",
-    }, */{
+    }, {
         "browserName": "android",
         "device": "Samsung Galaxy S9 Plus",
         "realMobile": true
@@ -47,7 +47,7 @@ exports.config = {
         "browserName": "android",
         "device": "Google Pixel 2",
         "realMobile": true
-    }],
+    }*/],
     seleniumAddress: "http://hub-cloud.browserstack.com/wd/hub",
     framework: "jasmine",
     jasmineNodeOpts: {
