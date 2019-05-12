c96635a2ddd73af9f4930b1aa4d8cb326c433846
Murat Demir <hisupermurat@gmail.com>
2019-05-12 11:32:33 +0300

    fix: e2e configuration for environment

---
+++ e2e/protractor.browserstack.prod.conf.js
@@ -0,0 +1,31 @@
+
+const { config } = require("./protractor.browserstack.base.conf.js");
+
+exports.config = {
+    ...config,
+    baseUrl: "https://supermurat.com/"
+};
+
+exports.config.multiCapabilities.push({
+    // Testing local site with Safari is kind of hard, so let`s test only live site with Safari for now
+    // https://www.browserstack.com/question/663
+    "browserName": "Safari",
+    "browser_version" : "10.1"
+}/*, {
+    // Testing on iPhone is not supported
+    // https://github.com/angular/protractor/issues/2840
+    // https://github.com/angular/protractor/issues/1736
+    // Error: Timeout - Async callback was not invoked within timeout specified by jasmine.DEFAULT_TIMEOUT_INTERVAL.
+    // https://www.browserstack.com/question/663
+    "browserName": "iPhone",
+    "device": "iPhone X",
+    "realMobile": true
+}*/);
+
+// Code to support common capabilities
+exports.config.multiCapabilities.forEach(function(caps){
+    Object.assign(caps, Object.assign({
+            "browserstack.local": false
+        },
+        exports.config.commonCapabilities, caps));
+});
--- e2e/protractor.browserstack.test.conf.js
+++ e2e/protractor.browserstack.test.conf.js
@@ -3,13 +3,14 @@ const { config } = require("./protractor.browserstack.base.conf.js");
 
 exports.config = {
     ...config,
-    baseUrl: "https://supermurat.com/"
+    baseUrl: "https://test.supermurat.com/"
 };
 
 exports.config.multiCapabilities.push({
     // Testing local site with Safari is kind of hard, so let`s test only live site with Safari for now
     // https://www.browserstack.com/question/663
-    "browserName": "Safari"
+    "browserName": "Safari",
+    "browser_version" : "10.1"
 }/*, {
     // Testing on iPhone is not supported
     // https://github.com/angular/protractor/issues/2840
