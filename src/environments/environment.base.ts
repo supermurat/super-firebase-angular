a02f58802d2a60e195a06939ff68e163e78e9964
Murat Demir <murat@kozayazilim.com>
2021-05-02 17:32:25 +0300

    feat: removed public keys from repo

--- src/environments/environment.base.ts
+++ src/environments/environment.base.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const environmentBase = {
     production: false,
     protocol: 'http://',
@@ -10,21 +13,7 @@ export const environmentBase = {
         'fb:app_id': undefined,
         'fb:admins': undefined
     },
-    Angulartics2: {
-        gst: {
-            trackingIds: ['UA-8121050-11']
-        }
-    },
-    firebase: {
-        apiKey: 'AIzaSyCBMHia-sEJ6srd8mnyfQqatMdZEtbyyqg',
-        authDomain: 'super-murat-beta.firebaseapp.com',
-        databaseURL: 'https://super-murat-beta.firebaseio.com',
-        projectId: 'super-murat-beta',
-        storageBucket: 'super-murat-beta.appspot.com',
-        messagingSenderId: '1049127022657',
-        appId: '1:1049127022657:web:11d999d7d785e02a'
-    },
-    cse: {
-        cx: '009513577206836808676:joyznri9mmm'
-    }
+    Angulartics2: JSON.parse(process.env.CONFIG_BASE_ANGULARTICS2),
+    firebase: JSON.parse(process.env.CONFIG_BASE_FIREBASE),
+    cse: JSON.parse(process.env.CONFIG_BASE_CSE)
 };
--- src/environments/environment.beta.ts
+++ src/environments/environment.beta.ts
@@ -5,3 +5,6 @@ export const environment = {...environmentBase};
 environment.production = true;
 environment.protocol = 'https://';
 environment.host = 'beta.supermurat.com';
+environment.Angulartics2 = JSON.parse(process.env.CONFIG_BETA_ANGULARTICS2);
+environment.firebase = JSON.parse(process.env.CONFIG_BETA_FIREBASE);
+environment.cse = JSON.parse(process.env.CONFIG_BETA_CSE);
