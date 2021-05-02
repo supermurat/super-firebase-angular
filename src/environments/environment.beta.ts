1d124ee7649c859a0f628e8d0f01383e6754b197
Murat Demir <murat@kozayazilim.com>
2021-05-02 18:49:13 +0300

    Revert "feat: removed public keys from repo"
    This reverts commit a02f58802d2a60e195a06939ff68e163e78e9964.

--- src/environments/environment.beta.ts
+++ src/environments/environment.beta.ts
@@ -5,6 +5,3 @@ export const environment = {...environmentBase};
 environment.production = true;
 environment.protocol = 'https://';
 environment.host = 'beta.supermurat.com';
-environment.Angulartics2 = JSON.parse(process.env.CONFIG_BETA_ANGULARTICS2);
-environment.firebase = JSON.parse(process.env.CONFIG_BETA_FIREBASE);
-environment.cse = JSON.parse(process.env.CONFIG_BETA_CSE);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,13 @@ export const environment = {...environmentBase};
 environment.production = true;
 environment.protocol = 'https://';
 environment.host = 'supermurat.com';
-environment.Angulartics2 = JSON.parse(process.env.CONFIG_PROD_ANGULARTICS2);
-environment.firebase = JSON.parse(process.env.CONFIG_PROD_FIREBASE);
-environment.cse = JSON.parse(process.env.CONFIG_PROD_CSE);
+environment.Angulartics2.gst.trackingIds = ['UA-8121050-6'];
+environment.firebase = {
+    apiKey: 'AIzaSyAt3I_Wnzm7_iMsXG0rM77FPAcTEAFCRHs',
+    authDomain: 'supermurat-com.firebaseapp.com',
+    databaseURL: 'https://supermurat-com.firebaseio.com',
+    projectId: 'supermurat-com',
+    storageBucket: 'supermurat-com.appspot.com',
+    messagingSenderId: '200768028544',
+    appId: '1:200768028544:web:afa4062f3e780479'
+};
